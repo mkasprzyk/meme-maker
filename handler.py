a57da450f5bf090a623a1941879f6aee08acc3ce
mkasprzyk <mkasprzyk@szy.fr>
2017-03-15 14:28:21 +0100

    Omfg / fix missing conflict markers

--- handler.py
+++ handler.py
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import logging
-
-from meme_maker.meme import Meme
-from meme_maker.cli import LOG_FORMAT
-
-logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
-logger = logging.getLogger('meme_handler')
-
-
-def meme_handler(event, context):
-    params = event.get('text').split(' ')
-
-    if len(params) >= 2:
-
-        template_or_url = params[1]
-        #TODO: urlparse instead of dumb string check
-        if template_or_url.startswitch('http'):
-            template = None
-            url = template_or_url
-        else:
-            template = template_or_url
-            url = None
-
-        text = ' '.join(params[1:])
-
-    else:
-        return 'Invalid meme-maker params. Try: /meme [template or url] [text...]'
-
-    meme = Meme(logger, template, url, text)
-    #meme.make_meme()
-    #TODO: return link to meme @ S3
-=======
 #!/usr/bin/env python
 
 import json
@@ -95,4 +62,3 @@ def handler(event, context):
     response_text = "@{}: here's your meme: {}".format(user_name, meme_url)
 
     return response(response_text, response_url)
->>>>>>> 3c76a3cf64e27dac3d9f84c31f69fa04c02b80e4
